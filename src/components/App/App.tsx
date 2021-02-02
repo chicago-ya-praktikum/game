b5a45940e27ba3632a83e7dca2b265c444aa0792
Anton <antongavrilov88@gmail.com>
2021-02-03 00:32:40 +0200

    linted commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Мой апп.</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>Мой апп.</h1>
+    </div>
+  );
 }
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,44 @@
-
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        historyApiFallback: true,
-        writeToDisk: true,
-        hot: true,
-        overlay: true,
-        open: true,
-        port: 9000
-    },
-    entry: "./src/index.tsx",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            },
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			}
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./www/index.html"
-        })
-    ]
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    writeToDisk: true,
+    hot: true,
+    overlay: true,
+    open: true,
+    port: 9000,
+  },
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './www/index.html',
+    }),
+  ],
 };
-
