f892cd8aee364d268c66a4275ffedb237b491a87
Anton <antongavrilov88@gmail.com>
2021-02-03 01:24:23 +0200

    fix linter

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Мой апп.</h1>
+      <h1>SOKOBAN</h1>
     </div>
   );
 }
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import sum from '../src/utils/sum.ts';
+import sum from '../src/utils/sum';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
