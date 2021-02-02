54dd9db7a0258d4e57412b768e44552317ad8e6b
Anton <antongavrilov88@gmail.com>
2021-02-02 22:57:02 +0200

    init config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        historyApiFallback: true,
+        writeToDisk: true,
+        hot: true,
+        overlay: true,
+        open: true,
+        port: 9000
+    },
+    entry: "./src/index.tsx",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./www/index.html"
+        })
+    ]
+};
+
