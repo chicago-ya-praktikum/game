472a186d6a44d69e896cdea494110e69e67f8376
Anton <antongavrilov88@gmail.com>
2021-02-08 11:18:24 +0200

    add no-semicolon rule

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   devServer: {
@@ -41,4 +41,4 @@ module.exports = {
       template: './www/index.html',
     }),
   ],
-};
+}
