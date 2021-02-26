e9fa73f033c784692af6b75458fc9b55325d9cbc
Anton <antongavrilov88@gmail.com>
2021-02-26 23:17:25 +0200

    add useCallback to LogOutButton

--- src/components/LogoutButton/LogOutButton.tsx
+++ src/components/LogoutButton/LogOutButton.tsx
@@ -8,11 +8,14 @@ import {actionCreator} from '../../utils/actionCreator'
 export const LogoutButton: FC = () => {
     const dispatch = useDispatch()
 
-    const userLogout = () => {
-        const getResponse = () => Auth.logout().then(() => dispatch(actionCreator(Actions.LOGOUT)))
-        return getResponse()
-    }
-
+    const userLogout = React.useCallback(
+        () => {
+            const getResponse = () => Auth.logout()
+                .then(() => dispatch(actionCreator(Actions.LOGOUT)))
+            return getResponse()
+        },
+        []
+    )
     return (
         <Button onClick={() => userLogout()}><Typography variant="h4" color="primary" gutterBottom>Выход</Typography></Button>
     )
