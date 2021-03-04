4af5d264b7160ecc7044985b741080a2930cc829
Anton <antongavrilov88@gmail.com>
2021-03-04 22:46:38 +0200

    change walls and box pictures

--- src/GameCore/models/dots/ClearDot.ts
+++ src/GameCore/models/dots/ClearDot.ts
@@ -2,6 +2,10 @@ import {Dot} from './Dot'
 
 export class ClearDot extends Dot {
     draw() {
-        this.ctx.clearRect(this.x, this.y, this.step, this.step)
+        const img = new Image()
+        img.src = './assets/gameElements/ground/ground.png'
+        img.onload = () => {
+            this.ctx.drawImage(img, this.x, this.y, this.step, this.step)
+        }
     }
 }
