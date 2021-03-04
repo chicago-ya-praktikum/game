8042fa8c9fcb2438a6b2444e45ac9f71691f51a2
Anton <antongavrilov88@gmail.com>
2021-03-04 22:25:10 +0200

    add player picture add wall pctures add target pictures

--- src/GameCore/models/dots/BoxDot.ts
+++ src/GameCore/models/dots/BoxDot.ts
@@ -2,7 +2,11 @@ import {Dot} from './Dot'
 
 export class BoxDot extends Dot {
     draw() {
-        const shift = (this.step - this.boxDiameter) / 2
-        this.ctx.strokeRect(this.x + shift, this.y + shift, this.boxDiameter, this.boxDiameter)
+        const img = new Image()
+        img.src = './assets/gameElements/box/box.png'
+        img.onload = () => {
+            const shift = (this.step - this.boxDiameter) / 2
+            this.ctx.strokeRect(this.x + shift, this.y + shift, this.boxDiameter, this.boxDiameter)
+        }
     }
 }
