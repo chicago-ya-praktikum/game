0d112db77b512bea42fb3c6e8db7e2c0cad2bc00
Anton <antongavrilov88@gmail.com>
2021-03-27 18:03:43 +0200

    add history and store to client

--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/style.sass'
-import {Provider} from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router';
+import { Provider as ReduxProvider } from 'react-redux';
+import 'babel-polyfill';
 import {App} from './components/App/App'
-import {store} from './store/store'
+import { configureStore } from './store/store';
+
+declare global {
+    interface Window {
+        __INITIAL_STATE__: any;
+    }
+}
+
+const initialState = window.__INITIAL_STATE__;
+const { store, history } = configureStore(initialState);
 
 ReactDOM.hydrate(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <ReduxProvider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </ReduxProvider>,
     document.getElementById('root')
 )
