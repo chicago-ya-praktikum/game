5fa14993756dcc6a926c83cb75bfad78196b31b0
Anton <antongavrilov88@gmail.com>
2021-03-27 17:22:13 +0200

    edit store and reducers

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,14 +1,16 @@
 import {combineReducers} from 'redux'
 // eslint-disable-next-line import/no-cycle
+import {connectRouter} from 'connected-react-router';
 import {userReducer} from './user/reducer'
 import {authReducer} from './auth/authReducer'
 import {themeReducer} from './gameThemeReduser'
 
 // В этом файле будем объединять все редьюсеры в один
-export const reducers = combineReducers({
+export const reducers = (history: any) => combineReducers({
     auth: authReducer,
     user: userReducer,
-    theme: themeReducer
+    theme: themeReducer,
+    router: connectRouter(history)
 })
 
 export type RootState = ReturnType<typeof reducers>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,29 @@
 import {applyMiddleware, createStore} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+import {createBrowserHistory, createMemoryHistory} from 'history';
+import {isServer} from 'src/utils/isServer';
 import {reducers} from './reducers'
 
-export function configureStore(preloadedState: {}) {
-    return createStore(reducers, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
+// export function configureStore(preloadedState: {}) {
+//     return createStore(reducers, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
+// }
+
+export function configureStore(preloadedState: {} | undefined, url = '/') {
+    const history = isServer
+        ? createMemoryHistory({initialEntries: [url]})
+        : createBrowserHistory();
+
+    const composeEnhancers = getComposeEnhancers();
+    const middlewares = [routerMiddleware(history), thunk];
+
+    const store = createStore(
+        createRootReducer(history),
+        preloadedState,
+        composeWithDevTools(applyMiddleware(...middlewares))
+    ) as AppStore;
+
+    return {store, history};
 }
 
 export const store = configureStore({})
