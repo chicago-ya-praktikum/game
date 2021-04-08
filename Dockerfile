539d18fad8f345fcef37407e0824cc7a94be39fa
Anton <antongavrilov88@gmail.com>
2021-04-08 20:09:59 +0200

    add ssr:build to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM node:13
 WORKDIR app
 COPY . .
 
-RUN npm install && npm run prod-ssr
+RUN npm install && npm run ssr:build
 
 EXPOSE 80
+
+CMD node SSR/index.js
--- src/webWorkers/levelGeneratorWorker.ts
+++ src/webWorkers/levelGeneratorWorker.ts
@@ -1,7 +1,9 @@
 import {sokobanGenerator} from '../SokobanGenerator/sokobanGenerator'
 
 // eslint-disable-next-line no-restricted-globals
-const ctx = self as unknown as Worker
+// const ctx = self as unknown as Worker
+// eslint-disable-next-line no-restricted-globals
+const ctx = self
 
 ctx.addEventListener('message', () => {
     const level = sokobanGenerator()
