73b3216277d9a33b6855de056028a67be05e3d77
Anton <antongavrilov88@gmail.com>
2021-04-10 17:27:35 +0200

    remove comments

--- api/app/models/index.ts
+++ api/app/models/index.ts
@@ -2,13 +2,10 @@
 const Sequelize = require('sequelize')
 import {sequelize} from '../config/db.config'
 import { userTable } from './user.model'
-// import {User} from './user.model'
 
 export const db:any = {}
 
 db.Sequelize = Sequelize
 db.sequelize = sequelize
 
-// db.users = new (User)(sequelize, Sequelize)
-// db.users = sequelize.define(User)
 db.users = userTable(sequelize, Sequelize)
