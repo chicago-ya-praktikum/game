4ced0f54e61d011658ec1cbd503842c1d2540ece
Anton <antongavrilov88@gmail.com>
2021-04-10 18:18:08 +0200

    fix db config bug - host

--- api/app/app.ts
+++ api/app/app.ts
@@ -2,15 +2,16 @@ const express = require("express");
 export const app = express();
 import {db} from './models/index'
 
-db.sequelize.sync();
+db.sequelize.sync().then(() => {
+}) 
 
-app.get("/", (req: any, res: { json: (arg0: { message: string; }) => void; }) => {
+app.get("/", (req: any, res: any) => {
     console.log(req)
-  res.json({ message: "Welcome to sokoban api" });
+  res.json({ message: "Welcome to sokoban api", db: db });
 });
 
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
