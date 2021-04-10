67aa2f15779db0c8671361c919480964886dd8b2
Anton <antongavrilov88@gmail.com>
2021-04-11 00:52:26 +0200

    remove useless comments

--- api/app/controllers/user.controller.ts
+++ api/app/controllers/user.controller.ts
@@ -3,11 +3,9 @@ import { db } from '../models/index'
 import { isUserData } from './requestDataVaidators';
 const User = db.users;
 const Token = db.tokens
-// const Op = db.Sequelize.Op;
 
-// Create and Save a new forum-user
+
 export const create = (req: any, res: any) => {
-    // Validate request
     if (!isUserData(req)) {
         res.status(400).send({
             message: "Wrong API"
@@ -15,7 +13,6 @@ export const create = (req: any, res: any) => {
         return;
     }
 
-    // Create a forum-user
     const user = {
         displayName: req.body.displayName,
         avatar: req.body.avatar
@@ -26,7 +23,6 @@ export const create = (req: any, res: any) => {
             if (data) {
                 res.status(409).send('User already in system')
             } else {
-                // Save forum user in the database
                 User.create(user)
                     .then((data: any) => {
                         const token = {
@@ -50,12 +46,8 @@ export const create = (req: any, res: any) => {
 
 };
 
-// Get all forum-users from the database.
 export const getAll = (req: any, res: any) => {
     console.log(req)
-    //   const title = req.query.title;
-    //   to do condition - user is online
-    //   var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
 
     User.findAll()
         .then((data: any) => {
