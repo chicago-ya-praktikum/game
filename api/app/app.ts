42ec5ebcdafc0ac819087e934dcaa50842aecb88
Anton <antongavrilov88@gmail.com>
2021-04-18 14:29:27 +0200

    remove semicolons

--- api/app/app.ts
+++ api/app/app.ts
@@ -2,10 +2,10 @@ import {db} from './models/index'
 import userRoutes from './routes/user.routes'
 import recordRoutes from './routes/record.routes'
 import reactionRoutes from './routes/reaction.routes'
-import userReactionsRoutes from './routes/userReaction.routes';
+import userReactionsRoutes from './routes/userReaction.routes'
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors');
 
 export const app = express()
@@ -18,14 +18,14 @@ app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
 
 db.sequelize.sync({force: true}).then(() => {
 })
 
 app.get('/', (req: any, res: any) => {
     console.log(req)
-    res.json({message: 'Welcome to sokoban api'});
+    res.json({message: 'Welcome to sokoban api'})
 })
 
 userRoutes(app)
@@ -36,5 +36,5 @@ userReactionsRoutes(app)
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`)
 });
