8001db305a62f834b64a3b7ae4fa91c55793c009
alex <alex@gmail.com>
2021-04-25 10:46:45 +0300

    Fix ssr build

--- src/webWorkers/levelGeneratorWorker.ts
+++ src/webWorkers/levelGeneratorWorker.ts
@@ -5,7 +5,7 @@ import {sokobanGenerator} from '../SokobanGenerator/sokobanGenerator'
 // eslint-disable-next-line no-restricted-globals
 const ctx = self
 
-ctx.addEventListener('message', ({data}: MessageEvent<number>) => {
-    const level = sokobanGenerator(data)
+ctx.addEventListener('message', (data) => {
+    const level = sokobanGenerator(data.data)
     ctx.postMessage(level)
 })
