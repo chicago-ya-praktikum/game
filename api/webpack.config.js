1016339bdf77305b25a2fde2170f75aa561da5ca
alex <alex@gmail.com>
2021-04-26 15:35:29 +0300

    game#74 Add docker compose

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const resolveCwd = require('resolve-cwd')
-let nodeExternals = require('webpack-node-externals');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     mode: 'development',
