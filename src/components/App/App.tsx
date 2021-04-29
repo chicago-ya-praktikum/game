ad5723751a7b846f580b36a9e72ee99126f5769c
alex <alex@gmail.com>
2021-04-29 15:22:46 +0300

    game#65 Fix strange name error

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,7 @@ import {AlertUI} from '../UI/Alert/AlertUI'
 import {OauthRedirect} from '../oauth/OauthRedirect/index'
 import {appThemeSelector} from '../../store/selectors'
 import {AppTheme} from '../../enums/AppTheme'
-import {Layout} from '../LayOut/Layout'
+import {Layout} from '../Layout/Layout'
 
 export function App() {
     const getTheme = (type: AppTheme) => createMuiTheme({
