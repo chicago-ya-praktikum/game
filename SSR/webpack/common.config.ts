f81c5ac3665013a35142184f7824824001a8a005
alex <alex@gmail.com>
2021-04-29 14:32:07 +0300

    game#65 Run prod and dev without changes

--- SSR/webpack/common.config.ts
+++ SSR/webpack/common.config.ts
@@ -1,5 +1,6 @@
 import webpack, {Configuration} from 'webpack'
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+import {IS_DEV} from './env'
 
 export const getCommonConfig  = (side: 'client' | 'server'): Configuration => {
     let IS_SERVER = side === 'server'
@@ -9,13 +10,14 @@ export const getCommonConfig  = (side: 'client' | 'server'): Configuration => {
         name: side,
         resolve: {
             plugins: [new TsconfigPathsPlugin({configFile: 'tsconfig.json'})]
-        },        
+        },
         plugins: [
             new webpack.DefinePlugin({
                 IS_CLIENT: JSON.stringify(IS_CLIENT),
                 IS_SERVER: JSON.stringify(IS_SERVER),
                 IS_SSR: JSON.stringify(true),
-                'typeof window': JSON.stringify(IS_CLIENT ? 'object' : 'undefined')
+                'typeof window': JSON.stringify(IS_CLIENT ? 'object' : 'undefined'),
+                IS_DEVELOPMENT: IS_DEV
             })
         ],
         devtool: 'source-map'
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,4 @@
 declare const IS_SERVER: boolean
 declare const IS_CLIENT: boolean
 declare const IS_SSR: boolean
+declare const IS_DEVELOPMENT: boolean
